4c2fc6b78ac33d526f8a9092df02db80c0379243
David MacCallum <david.maccallum@takealot.com>
2021-10-15 08:13:27 +0200

    Script

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ const yargs = require('yargs')
 yargs
   .commandDir('cmds')
   .demandCommand(1, 'Please choose a command')
-  .completion('completion', 'Enable bash/zsh-completion shortcuts for commands and options.')
   .help()
   .argv
